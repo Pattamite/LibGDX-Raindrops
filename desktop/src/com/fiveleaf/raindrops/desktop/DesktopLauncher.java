79a4fc6605ca1c264ee00f5c379e034ba5a88b6b
Pattnatt <jittapat007@gmail.com>
2016-10-10 10:26:24 +0700

    change title

--- desktop/src/com/fiveleaf/raindrops/desktop/DesktopLauncher.java
+++ desktop/src/com/fiveleaf/raindrops/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.fiveleaf.raindrops.RaindropsGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "Raindrops Keep Fallin' on My Cacti!";
+        config.title = "Raindrops Keep Fallin' on My Plant!";
         config.width = 160;
 		config.height = 144;
         new LwjglApplication(new RaindropsGame(), config);
